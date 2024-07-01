4ff0aabf5beea680aef2a0c73d6ca2f35674ad8e
Lekshmi <lekshmi.anilkumar@Shellsquare.com>
2024-07-01 20:05:30 +0530

    Added vue-argon-dashboard as a submodule

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <App></App> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import App from './components/AdminTemplate/vue-argon-design-system-master/src/App.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld
+    App
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ArgonDashboard from './plugins/argon-dashboard';
+import { BootstrapVue } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+// import Argon from '@/plugins/argon-kit'
+// import router from "./starterRouter";
+
+
+// Vue.use(Argon);
+// import Argon from './components/AdminTemplate';
+// import './components/argon-design-system/assets/vendor/nucleo/css/nucleo.css';
+// import './components/argon-design-system/assets/vendor/font-awesome/css/font-awesome.css';
+// import './components/argon-design-system/assets/scss/argon.scss';
 
 Vue.config.productionTip = false
+// Vue.use(Argon);
+Vue.use(BootstrapVue);
+
+Vue.use(ArgonDashboard);
 
 new Vue({
   render: h => h(App),
